e221f9464388c22c9ce2e821eddac8e3e8ec9102
maxmvk <maxmvk1993@gmail.com>
2020-11-13 17:49:23 +0200

    feat(beta): created separate components, screens, added animation

---
+++ src/components/home/beta/promotions/promotions.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./promotions.module.scss";
+import Slide from 'react-reveal/Slide';
+
+const Promotions = () => (
+  <Slide right duration={500}>
+    <h1>Promotions</h1>
+    <div className={styles.container}>
+      <div className={styles.promotion}>
+        <div></div>
+        <p>Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus.</p>
+      </div>
+      <div className={styles.promotion}>
+        <div></div>
+        <p>Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus.</p>
+      </div>
+      <div className={styles.promotion}>
+        <div></div>
+        <p>Adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus.</p>
+      </div>
+    </div>
+  </Slide>
+)
+
+export default Promotions;
